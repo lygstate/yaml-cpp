c8dd066d11d4c8601061c9b7f632fcb5eed07099
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-11-18 22:46:46 +0100

    use custom shared pointer
    - simplify memory management

--- include/yaml-cpp/node/ptr.h
+++ include/yaml-cpp/node/ptr.h
@@ -8,19 +8,81 @@
 #endif
 
 #include "yaml-cpp/dll.h"
-#include <memory>
 
 namespace YAML {
 namespace detail {
-class node;
-class node_data;
-class memory;
-class memory_holder;
 
-typedef std::shared_ptr<node> shared_node;
-typedef std::shared_ptr<node_data> shared_node_data;
-typedef std::shared_ptr<memory_holder> shared_memory_holder;
-typedef std::shared_ptr<memory> shared_memory;
+template<typename T>
+struct ref_holder {
+    T *n;
+    ~ref_holder<T>() {
+        if (n) n->release();
+    }
+    ref_holder<T>(T* ptr) {
+        n = ptr;
+    }
+    ref_holder<T>(const ref_holder<T>& ref) {
+        if (ref.n) {
+            ref.n->hold();
+        }
+        n = ref.n;
+    }
+    ref_holder<T>(ref_holder<T>&& ref) {
+        n = ref.n;
+        ref.n = nullptr;
+    }
+    ref_holder<T>& operator=(const ref_holder<T>& ref) {
+        if (ref.n == n) { return *this; }
+
+        if (ref.n) { ref.n->hold(); }
+        if (n) { n->release(); }
+        n = ref.n;
+        return *this;
+    }
+    ref_holder<T>& operator=(ref_holder<T>&& ref) {
+        if (ref.n == n) { return *this; }
+
+        if (n) { n->release(); }
+        n = ref.n;
+        ref.n = nullptr;
+
+        return *this;
+    }
+    bool operator<(const ref_holder<T>& ref) const { return n < ref.n; }
+    bool operator==(const ref_holder<T>& ref) const { return n == ref.n; }
+
+    const T* operator->() const { return n; }
+    T* operator->() { return n; }
+
+    const T& operator*() const { return *n; }
+    T& operator*() { return *n; }
+
+    const T* get() { return n; }
+
+    void reset(T* _n) {
+        if (_n == n) { return; }
+        if (n) { n->release(); }
+        n = _n;
+    }
+
+    operator bool() const { return n != nullptr; }
+};
+
+struct ref_counted {
+
+  void hold() { refs++; }
+  void release() {
+      if (--refs == 0) {
+          delete this;
+      }
+  }
+  virtual ~ref_counted() {}
+
+private:
+    std::size_t refs = 1;
+
+};
+
 }
 }
 
